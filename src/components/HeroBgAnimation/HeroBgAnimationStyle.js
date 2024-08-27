592f5b8b1b033f6441f0aebd8bb3ea79cecf1884
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-08-27 11:50:03 +0530

    Update HeroBgAnimationStyle.js formatting and remove unused code

--- src/components/HeroBgAnimation/HeroBgAnimationStyle.js
+++ src/components/HeroBgAnimation/HeroBgAnimationStyle.js
@@ -1,25 +1,6 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Div = styled.div`
-    width: 600px;
-    height: 500px;
-    position: absolute;
-    top: 0;
-    right: 0;   
-    bottom: 0;
-    left: 50%;
-    z-index: 1;
-
-    @media screen and (max-width: 960px) {
-        position: static;
-        width: 600px;
-        height: 700px;
-    }
-
-    @media screen and (max-width: 640px) {
-        position: static;
-        width: 400px;
-        height: 500px;
-    }
-    `;
+  width: 600px;
+  height: 500px;
+`;

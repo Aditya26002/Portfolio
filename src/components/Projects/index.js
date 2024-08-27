982cf76df176591ddc98aa0c219870c6970a5dd0
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-08-27 11:50:39 +0530

    Update Projects component with styled components and optimize imports

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,100 +1,106 @@
 import React from "react";
-import styled from "styled-components";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
 
-const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap; /* Enable wrapping */
-  justify-content: center;
-  position: relative;
-  z-index: 1;
-  align-items: center;
-`;
-
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap; /* Enable wrapping for responsive layout */
-  justify-content: center; /* Center content horizontally */
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  gap: 12px;
-`;
-
-export const Title = styled.div`
-  font-size: 42px;
-  text-align: center;
-  font-weight: 600;
-  margin-top: 20px;
-  color: ${({ theme }) => theme.text_primary};
-  @media (max-width: 768px) {
-    margin-top: 12px;
-    font-size: 32px;
-  }
-`;
-
-export const Desc = styled.div`
-  font-size: 18px;
-  text-align: center;
-  max-width: 600px;
-  color: ${({ theme }) => theme.text_secondary};
-  @media (max-width: 768px) {
-    font-size: 16px;
-  }
-`;
-
-const ToogleGroup = styled.div`
-  display: flex;
-  border: 1.5px solid ${({ theme }) => theme.primary};
-  color: ${({ theme }) => theme.primary};
-  border-radius: 12px;
-  font-weight: 500;
-  margin: 22px 0;
-
-  @media (max-width: 768px) {
-    font-size: 12px;
-  }
-`;
-
-const ToggleButton = styled.button`
-  padding: 8px 18px;
-  cursor: pointer;
-  border-radius: 6px;
-
-  ${({ active }) =>
-    active &&
-    `
-    background-color: ${({ theme }) => theme.primary + 20};  
-    `}
-
-  &:hover {
-    background-color: ${({ theme }) => theme.primary + 8};
-  }
-
-  @media (max-width: 768px) {
-    padding: 6px 8px;
-    border-radius: 4px;
-  }
-`;
-
-const Divider = styled.div`
-  width: 1.5px;
-  background-color: ${({ theme }) => theme.primary};
-`;
-
-const Projects = () => {
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
   return (
-    <Container>
+    <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
-        <Desc>Here are some of the projects I have worked on.</Desc>
-
-        <ToogleGroup>
-          <ToggleButton>All</ToggleButton>
-          <ToggleButton>Website</ToggleButton>
-          <ToggleButton>Application</ToggleButton>
+        <Desc>
+          I have worked on a wide range of projects. From web apps to android
+          apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          )}
           <Divider />
-        </ToogleGroup>
+          {toggle === "web app" ? (
+            <ToggleButton
+              active
+              value="web app"
+              onClick={() => setToggle("web app")}
+            >
+              WEB APP'S
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
+              WEB APP'S
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "android app" ? (
+            <ToggleButton
+              active
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              ANDROID APP'S
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              ANDROID APP'S
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "machine learning" ? (
+            <ToggleButton
+              active
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              MACHINE LEARNING
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              MACHINE LEARNING
+            </ToggleButton>
+          )}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
       </Wrapper>
     </Container>
   );
